b1a79adf60ae0b0ddb3dc55b3f10ff4eb5a94569
gyesayan <75162261+gyesayan@users.noreply.github.com>
2020-11-30 23:54:33 +0400

    Add files via upload

---
+++ ft_print_comb.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gyesayan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/30 19:11:04 by gyesayan          #+#    #+#             */
+/*   Updated: 2020/11/30 23:51:03 by gyesayan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	putcomma(char i)
+{
+	if (i != '7')
+		write(1, ", ", 2);
+}
+
+void	ft_print_comb(void)
+{
+	char i;
+	char j;
+	char k;
+
+	i = '0';
+	while (i < '8')
+	{
+		j = i + 1;
+		while (j < '9')
+		{
+			k = j + 1;
+			while (k <= '9')
+			{
+				write(1, &i, 1);
+				write(1, &j, 1);
+				write(1, &k, 1);
+				k++;
+				putcomma(i);
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+int		main(void)
+{
+	ft_print_comb();
+	return (0);
+}
